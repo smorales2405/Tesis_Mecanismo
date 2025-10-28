1c6c2febcdebb54f9c1962a9b72ac78e29ccfcb9
smorales2405 <smorales@utec.edu.pe>
2025-10-28 02:37:54 -0500

    Interrupt

--- Arduino/tests/lectura_encoder/lectura_encoder.ino
+++ Arduino/tests/lectura_encoder/lectura_encoder.ino
@@ -2,12 +2,13 @@
 
 // Timer para ESP32
 hw_timer_t * timer = NULL;
-volatile bool timer_flag = false;
+portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
 
 // Constantes
 const float Pi = 3.14159265;
 const uint16_t cuentas_max = 8400;
-const float dt = 2.5; //Milisegundos
+const float dt = 1; //Milisegundos
+const int freq_timer = 1000000 / (dt * 1000);
 
 // Variables lectura encoder
 volatile int32_t cuentas = 0;
@@ -29,20 +30,61 @@ const int ledChannel = 0;  // Canal PWM
 const int resolution = 8;  // Resolución de 8 bits (0-255)
 int PWM = 0;
 
-void IRAM_ATTR lectura_encoder();
-void IRAM_ATTR ISR_Timer();
+void IRAM_ATTR ISR_Timer()
+{      
+  portENTER_CRITICAL_ISR(&timerMux);  
+  current_position = (cuentas*2*Pi)/cuentas_max; 
+  velocidad = ((current_position - last_position)/(dt)*1000);
+  velocidad = meanFilter.AddValue(velocidad);
+  velocidadf = abs(velocidad);
+  
+  last_position = current_position;
+  Serial.println("ISR_Timer");
+  portEXIT_CRITICAL_ISR(&timerMux);
+    
+}
+
+void IRAM_ATTR lectura_encoder()
+{
+    if(digitalRead(Pin_encoder_A)==1)
+      bitSet(current_state,1);
+    else
+      bitClear(current_state,1);
+     
+    if(digitalRead(Pin_encoder_B)==1)
+      bitSet(current_state,0);
+    else
+      bitClear(current_state,0);
+  
+    if(last_state==3 && current_state==1)
+      cuentas++;
+    if(last_state==1 && current_state==0) 
+      cuentas++;
+    if(last_state==0 && current_state==2)
+      cuentas++;
+    if(last_state==2 && current_state==3)
+      cuentas++;   
+       
+    if(last_state==3 && current_state==2)
+      cuentas--;
+    if(last_state==2 && current_state==0)
+      cuentas--;
+    if(last_state==0 && current_state==1)
+      cuentas--;
+    if(last_state==1 && current_state==3)
+      cuentas--;
+      
+    last_state = current_state; 
+}
 
 void setup()  
 {
-    Serial.begin(9600); 
+    Serial.begin(115200);
     
     pinMode(Pin_encoder_A, INPUT); 
     pinMode(Pin_encoder_B, INPUT); 
     pinMode(Pin_DIR, OUTPUT); 
     pinMode(Pin_PWM, OUTPUT);
-
-    // Configurar PWM para ESP32
-    ledcAttachChannel(Pin_PWM, freq, resolution, ledChannel);
     
     // Configurar interrupciones del encoder
     attachInterrupt(digitalPinToInterrupt(Pin_encoder_A), lectura_encoder, CHANGE);
@@ -50,9 +92,12 @@ void setup()
     
     // Configurar Timer para ESP32 (nueva API v3.x)
     // Frecuencia = 1000000 / (dt * 1000) = 1000 / dt Hz
-    timer = timerBegin(1000000 / (dt * 1000));  // Frecuencia en Hz
+    timer = timerBegin(freq_timer);  // Frecuencia en Hz
     timerAttachInterrupt(timer, &ISR_Timer);
-    timerStart(timer);
+    timerAlarm(timer, 100, true, 0);
+
+    // Configurar PWM para ESP32
+    ledcAttach(Pin_PWM, freq, resolution);
 }
 
 void loop()
@@ -116,49 +161,4 @@ void loop()
   Serial.print(",");
   Serial.println(velocidad);
   delay(5000);
-}
-
-void IRAM_ATTR ISR_Timer()
-{      
-    
-  current_position = (cuentas*2*Pi)/cuentas_max; 
-  velocidad = ((current_position - last_position)/(dt)*1000);
-  velocidad = meanFilter.AddValue(velocidad);
-  velocidadf = abs(velocidad);
-  
-  last_position = current_position;
-    
-}
-
-void IRAM_ATTR lectura_encoder()
-{
-    if(digitalRead(Pin_encoder_A)==1)
-      bitSet(current_state,1);
-    else
-      bitClear(current_state,1);
-     
-    if(digitalRead(Pin_encoder_B)==1)
-      bitSet(current_state,0);
-    else
-      bitClear(current_state,0);
-  
-    if(last_state==3 && current_state==1)
-      cuentas++;
-    if(last_state==1 && current_state==0) 
-      cuentas++;
-    if(last_state==0 && current_state==2)
-      cuentas++;
-    if(last_state==2 && current_state==3)
-      cuentas++;   
-       
-    if(last_state==3 && current_state==2)
-      cuentas--;
-    if(last_state==2 && current_state==0)
-      cuentas--;
-    if(last_state==0 && current_state==1)
-      cuentas--;
-    if(last_state==1 && current_state==3)
-      cuentas--;
-      
-    last_state = current_state; 
 }
