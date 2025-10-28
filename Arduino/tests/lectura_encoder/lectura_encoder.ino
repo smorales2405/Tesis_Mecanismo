927b3b7c376a833b1a043b5ccaa95abfacb37b93
smorales2405 <smorales@utec.edu.pe>
2025-10-28 01:56:14 -0500

    updt

--- Arduino/tests/lectura_encoder/lectura_encoder.ino
+++ Arduino/tests/lectura_encoder/lectura_encoder.ino
@@ -1,13 +1,13 @@
 #include <MeanFilterLib.h>
 
-// Agregar después de #include <MeanFilterLib.h>
-hw_timer_t *Timer = NULL;
-portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
+// Timer para ESP32
+hw_timer_t * timer = NULL;
+volatile bool timer_flag = false;
 
 // Constantes
 const float Pi = 3.14159265;
 const uint16_t cuentas_max = 8400;
-const float dt = 2.5;
+const float dt = 2.5; //Milisegundos
 
 // Variables lectura encoder
 volatile int32_t cuentas = 0;
@@ -18,33 +18,41 @@ MeanFilter<float> meanFilter(10);
 float current_position = 0.0, last_position = 0.0;
 float velocidad = 0.0;
 double velocidadf = 0.0;
-int PWM = 0;
 
 // Pines
 int Pin_encoder_A = 16, Pin_encoder_B = 17;
 int Pin_DIR = 32, Pin_PWM = 33;
+
+// Configuración PWM para ESP32
+const int freq = 7812;  // Frecuencia PWM en Hz
+const int ledChannel = 0;  // Canal PWM
+const int resolution = 8;  // Resolución de 8 bits (0-255)
+int PWM = 0;
 
 void IRAM_ATTR lectura_encoder();
 void IRAM_ATTR ISR_Timer();
 
 void setup()  
 {
-	Serial.begin(9600); 
-	
-	pinMode(Pin_encoder_A,INPUT_PULLUP); 
-	pinMode(Pin_encoder_B,INPUT_PULLUP);
+    Serial.begin(9600); 
+    
+    pinMode(Pin_encoder_A, INPUT); 
+    pinMode(Pin_encoder_B, INPUT); 
+    pinMode(Pin_DIR, OUTPUT); 
+    pinMode(Pin_PWM, OUTPUT);
 
-  pinMode(Pin_DIR,OUTPUT); 
-  pinMode(Pin_PWM,OUTPUT); 
-	
-  attachInterrupt(digitalPinToInterrupt(Pin_encoder_A), lectura_encoder, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(Pin_encoder_B), lectura_encoder, CHANGE);
-
-  // Configurar timer: frecuencia = 1MHz / (dt*1000) = 1000000/2500 = 400 Hz
-  Timer = timerBegin(1000000);  // 1 MHz (1 microsegundo de resolución)
-  timerAttachInterrupt(Timer, &ISR_Timer);
-  timerAlarm(Timer, dt * 1000, true, 0);  // 2500 µs, auto-reload, unlimited
-
+    // Configurar PWM para ESP32
+    ledcAttachChannel(Pin_PWM, freq, resolution, ledChannel);
+    
+    // Configurar interrupciones del encoder
+    attachInterrupt(digitalPinToInterrupt(Pin_encoder_A), lectura_encoder, CHANGE);
+    attachInterrupt(digitalPinToInterrupt(Pin_encoder_B), lectura_encoder, CHANGE);
+    
+    // Configurar Timer para ESP32 (nueva API v3.x)
+    // Frecuencia = 1000000 / (dt * 1000) = 1000 / dt Hz
+    timer = timerBegin(1000000 / (dt * 1000));  // Frecuencia en Hz
+    timerAttachInterrupt(timer, &ISR_Timer);
+    timerStart(timer);
 }
 
 void loop()
@@ -54,65 +62,76 @@ void loop()
   analogWrite(Pin_PWM,PWM);
   Serial.print(PWM); 
   Serial.print(",");
+  Serial.print(cuentas); 
+  Serial.print(",");
   Serial.println(velocidad);
-  delay(500);
+  delay(5000);
 
   PWM = 255*0.5;
   digitalWrite(Pin_DIR,LOW);      
   analogWrite(Pin_PWM,PWM);
   Serial.print(PWM); 
   Serial.print(",");
+  Serial.print(cuentas); 
+  Serial.print(",");
   Serial.println(velocidad);
-  delay(500);
+  delay(5000);
 
   PWM = 255*1;
   digitalWrite(Pin_DIR,LOW);      
   analogWrite(Pin_PWM,PWM);
   Serial.print(PWM); 
   Serial.print(",");
+  Serial.print(cuentas); 
+  Serial.print(",");
   Serial.println(velocidad);
-  delay(500);
+  delay(5000);
 
   PWM = 255*0.25;
   digitalWrite(Pin_DIR,HIGH);      
   analogWrite(Pin_PWM,PWM);
   Serial.print(PWM); 
   Serial.print(",");
+  Serial.print(cuentas); 
+  Serial.print(",");
   Serial.println(velocidad);
-  delay(500);
+  delay(5000);
 
   PWM = 255*0.5;
   digitalWrite(Pin_DIR,HIGH);      
   analogWrite(Pin_PWM,PWM);
   Serial.print(PWM); 
   Serial.print(",");
+  Serial.print(cuentas); 
+  Serial.print(",");
   Serial.println(velocidad);
-  delay(500);
+  delay(5000);
 
   PWM = 255*1;
   digitalWrite(Pin_DIR,HIGH);      
   analogWrite(Pin_PWM,PWM);
   Serial.print(PWM); 
   Serial.print(",");
+  Serial.print(cuentas); 
+  Serial.print(",");
   Serial.println(velocidad);
-  delay(500);
+  delay(5000);
 }
 
 void IRAM_ATTR ISR_Timer()
 {      
-
-    current_position = (cuentas*2*Pi)/cuentas_max; 
-    velocidad = ((current_position - last_position)/(dt)*1000);
-    velocidad = meanFilter.AddValue(velocidad);
-    velocidadf = abs(velocidad);
     
-    last_position = current_position;
-
+  current_position = (cuentas*2*Pi)/cuentas_max; 
+  velocidad = ((current_position - last_position)/(dt)*1000);
+  velocidad = meanFilter.AddValue(velocidad);
+  velocidadf = abs(velocidad);
+  
+  last_position = current_position;
+    
 }
 
 void IRAM_ATTR lectura_encoder()
-{    
-
+{
     if(digitalRead(Pin_encoder_A)==1)
       bitSet(current_state,1);
     else
@@ -142,5 +161,4 @@ void IRAM_ATTR lectura_encoder()
       cuentas--;
       
     last_state = current_state; 
-
 }
